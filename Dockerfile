798c81e2d154b24ad94e35aea985e1d9da5e742d
WillR247 <billyroberts247@gmail.com>
2022-05-03 17:46:43 +0100

    Updated mySQL to use utf8, added add_patient function

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.8
 WORKDIR .
 COPY requirements.txt .
+COPY data ./data
 RUN python -m pip install -r requirements.txt
 COPY *.py .
 CMD ["python","./process_json.py"]
--- process_json.py
+++ process_json.py
@@ -1,5 +1,6 @@
 import json
 import mysql.connector
+import os
 
 def connect_to_database(db):
     try:
@@ -64,6 +65,101 @@ def create_database():
         new_con.close()
         print("Successfully created patient database!\n")
 
+def add_patient(patient_data):
+    con = connect_to_database("patient_data")
+    db_cursor = con.cursor()
+
+    # Sets the more complex data points to variables to reduce SQL complexity
+    first_name = patient_data['name'][0]['given'][0]
+    surname = patient_data['name'][0]['family']
+    try:
+        prefix = patient_data['name'][0]['prefix'][0]
+    except:
+        prefix = ""
+
+    uuid = patient_data['id']
+    birth_date = patient_data['birthDate']
+    gender = patient_data['gender']
+    birth_sex = patient_data['extension'][3]['valueCode']
+    mother_maiden = patient_data['extension'][2]['valueString']
+    marital_status = patient_data['maritalStatus']['text']
+    birth_city = patient_data['extension'][4]['valueAddress']['city']
+    birth_state = patient_data['extension'][4]['valueAddress']['state']
+    birth_country = patient_data['extension'][4]['valueAddress']['country']
+    ethnicity = patient_data['extension'][1]['extension'][1]['valueString']
+    race = patient_data['extension'][0]['extension'][1]['valueString']
+    try:
+        death_datetime = patient_data['deceasedDateTime']
+    except:
+        death_datetime = "N/A"
+
+    address_line = patient_data['address'][0]['line'][0]
+    address_city = patient_data['address'][0]['city']
+    address_state = patient_data['address'][0]['state']
+    address_country = patient_data['address'][0]['country']
+    address_lat = patient_data['address'][0]['extension'][0]['extension'][0]['valueDecimal']
+    address_long = patient_data['address'][0]['extension'][0]['extension'][1]['valueDecimal']
+
+    phone_number = patient_data['telecom'][0]['value']
+    language = patient_data['communication'][0]['language']['text']
+
+    db_cursor.execute("INSERT INTO patient (uuid, "
+    "first_name, surname, prefix, "
+    "gender, birth_sex, birth_date, "
+    "address_line, address_city, address_state, address_country, address_lat, address_long, "
+    "birth_city, birth_state, birth_country, "
+    "ethnicity, race, mother_maiden, marital_status, "
+    "phone_number, language, "
+    "death_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",[
+        uuid,
+        first_name, surname, prefix, 
+        gender, birth_sex, birth_date, 
+        address_line, address_city, address_state, address_country, address_lat, address_long,
+        birth_city, birth_state, birth_country, 
+        ethnicity, race, mother_maiden, marital_status, 
+        phone_number, language, 
+        death_datetime])
+    return "temp"
+
+def process_json_data(json_data, file_name):
+    # Verifies that the file is using the correct format
+    if json_data['entry'][0]['resource']['resourceType'] == "Patient":
+        patient_data = json_data['entry'][0]['resource']
+
+        # Creates a connection to the database, and checks if the patient's unique ID is already in the patient table, to avoid duplication
+        con = connect_to_database("patient_data")
+        db_cursor = con.cursor()
+        db_cursor.execute("SELECT * FROM patient WHERE uuid = %s",[patient_data['id']])
+
+        if len(db_cursor.fetchall()) == 0:
+            #Send data to add_patient function, return the patient id
+            db_cursor.close()
+            patient_id = add_patient(patient_data)
+
+            for entry in json_data['entry'][1:]:
+                event_data = entry['resource']
+                # add_patient_event(event_data, patient_id)
+
+        else:
+            print("Patient data already stored in database\n")
+    else:
+        print("ERROR: File {} is using an incorrect format\n".format(file_name))
+
+def load_json_data():
+    # Loads the filenames in the data directory into an array
+    files = os.listdir("data")
+    print("Loading {} files from data directory...".format(len(files)))
+
+    # Opens each file, sending the json data contained to the process_json_data function
+    for file in files:
+        with open("data/{}".format(file), "r") as f:
+            print("Processing file - {}".format(file))
+            try:
+                data = f.read()
+                process_json_data(json.loads(data), file)
+            except:
+                print("ERROR: Could not process file {}\n".format(file))
+
 con = mysql.connector.connect(
             host="mysqldb",
             user="root",
@@ -72,3 +168,4 @@ con = mysql.connector.connect(
 db_cursor = con.cursor()
 db_cursor.execute("DROP DATABASE patient_data")
 create_database()
+load_json_data()
